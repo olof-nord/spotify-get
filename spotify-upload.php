3cb038d071e6fd43ce1396bf7cbc5e408f789299
Olof Nord <olof_nord_@hotmail.com>
2011-03-08 22:52:35 +0100

    worked some more, next up will be to embed a youtubesearch for the artist and song

--- spotify-upload.php
+++ spotify-upload.php
@@ -6,26 +6,45 @@ if (isset($_POST["spotify_url"]))
 {
     if (empty($_POST["spotify_url"]))
         {
-            echo "du måste skriva något...";
-            break 2;
+		echo "du måste skriva något...";
         }
     elseif (substr($_POST["spotify_url"],0,29)=="http://open.spotify.com/track/")
         {
-            echo "det måste vara en låt, inget annat";
-            break 2;
+		echo "det måste vara en låt, inget annat";
         }        
 
+    elseif (substr($_POST["spotify_url"],0,29)=="If you enable JavaScript in your browser,")
+	{
+		echo "something went wrong, you got the js error again";
+	}
     else
-    {
-    //exempelurl: 'http://open.spotify.com/track/2lN6G35gsXkA3xzPYqmis5';
-    $spotifyurl = file($_POST["spotify_url"]);
+    	{
+    	//exempelurl: 'http://open.spotify.com/track/2lN6G35gsXkA3xzPYqmis5';
+    	$spotifyurl = file($_POST["spotify_url"]);
 
-    echo "Title:<br/>";
-    echo $spotifyurl[56];
-    echo "<br/>";
-    echo "Artist:<br/>";
-    //First substr to remove last " and second substr to remove initial garbage
-    echo substr(substr($spotifyurl[63],31), 0,-2);
-    }
+	$songtitle=$spotifyurl[56];
+        //First substr to remove last " and second substr to remove initial garbage
+        $artist= substr(substr($spotifyurl[63],31), 0,-2);
+
+
+    	echo "Title:<br/>";
+   	echo $songtitle;
+    	echo "<br/>";
+
+    	echo "Artist:<br/>";
+    	echo $artist;
+
+	echo "<br/>";
+        echo "http://www.youtube.com/results?search_query=".$artist.$songtitle;
+
+        echo "<p>Youtubepage hopefully containing somehting relevant to the link</p>";
+        //echo "<iframe src="http://www.youtube.com/results?search_query="$artist.$songtitle" width="150px" height="200px"></iframe>";
+        
+        echo "<br/>";
+        //echo "<a href="http://kg-tm04/olonor9215/PHP/spotify-get/">return @ school</a>";
+	//echo "<a href="http://localhost/spotify-get/index.htm">return @ home</a>;
+
+
+   	}
 }
 ?>
